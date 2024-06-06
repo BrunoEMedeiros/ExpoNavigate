cff08a3e88ce282e7503930e4e8209fbdc53c544
D3adSec <bruno.emedeiros99@gmail.com>
2024-06-06 14:15:08 -0300

    consumindo get

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,12 +1,38 @@
-import { View, Text } from "react-native";
+import { apiConfig } from "@/utils/api";
+import { useEffect, useState } from "react";
+import { View, Text, FlatList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+
+interface Salas{
+    id: number,
+    nome: string,
+    ativo: number
+}
 
 export default function Home()
 {
+    const [salas, setSalas] = useState<Salas[]>([])
+
+    //Toda vez que voce quiser que algo aconteça assim que,
+    //o component é carregado na tela, use useEffect()
+    useEffect(() => {
+        apiConfig.get('/salas')
+        .then((res)=>{
+            setSalas([...res.data])
+        })
+    }, [salas]);
+
     return(
         <SafeAreaView>
             <View>
-                <Text>Pagina principal</Text>
+               <FlatList 
+                    data={salas}
+                    keyExtractor={item => item.id.toString()}
+                    horizontal={true}
+                    renderItem={({item}) =>{
+                        return <Text>{item.nome}</Text>
+                    }}
+               />
             </View>
         </SafeAreaView>
     )
